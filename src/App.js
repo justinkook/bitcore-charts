2f83a8e36c3cca232648ce75749ecaad0460f2c7
Justin <justin@Satoshis-MacBook-Pro.local>
2019-01-23 13:31:19 -0500

    finished demo functionality

--- src/App.js
+++ src/App.js
@@ -2,37 +2,70 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import { SnackbarProvider, withSnackbar } from 'notistack';
+import MediaControlCard from './components/BalanceCard';
+import axios from 'axios';
 import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3000', { transports: ['websocket'] });
 
 class App extends React.Component {
   state = {
-      message: 'This is a success message!'
+      message: 'This is a success message!',
+      balance: 0,
   }
   componentDidMount = () => {
     socket.on('connect', () => {
         console.log('Connected to socket');
         socket.emit('room', '/BTC/regtest/inv');
-        this.streamEvent();
+        this.handleGetBalance();
+        this.handleBlockEvent();
+        this.handleTxEvent();
         });
   }
 
-  streamEvent = () => {
+  handleBlockEvent = () => {
     socket.on('block', payload => {
-            let message = `Recieved ${payload.reward/100000000} BTC at ${new Date(payload.time).toLocaleString()}`;
+            let message = `Recieved Block Reward ${payload.reward/100000000} BTC at ${new Date(payload.time).toLocaleString()}`;
             this.setState({message: message});
             this.handleClickVariant('success')();
+            this.handleGetBalance();
         })
+  }
+
+  handleTxEvent = () => {
+    socket.on('tx', payload => {
+      let message = `Fee of -${payload.fee/100000000} BTC at ${new Date(payload.blockTimeNormalized).toLocaleString()}`;
+      this.setState({message: message});
+      this.handleClickVariant('error')();
+      message = `Recieved ${payload.value/100000000} BTC at ${new Date(payload.blockTimeNormalized).toLocaleString()}`;
+      this.setState({message: message});
+      this.handleClickVariant('success')();
+      this.handleGetBalance();
+  })
+  }
+
+  handleGetBalance = () => {
+    axios.get('http://localhost:3000/api/BTC/regtest/address/mnL48Dja4zP9WU4wN6mwmnyJJLyuzoW88x/balance')
+    .then(res => {
+      this.setState({balance: res.data.balance/100000000})
+    })
   }
 
   handleClickVariant = variant => () => {
     this.props.enqueueSnackbar(this.state.message, { variant });
   };
 
+  componentWillUnmount = () => {
+    this.handleBlockEvent.destroy()
+    this.handleTxEvent.destroy()
+    this.handleClickVariant.destroy()
+    this.handleGetBalance.destroy()
+  }
+
   render() {
     return (
       <React.Fragment>
         <Button onClick={this.handleClickVariant('success')}>Generate a Block on Bitcore RegTest!</Button>
+        <MediaControlCard balance={this.state.balance} />
       </React.Fragment>
     );
   }
---
+++ src/components/BalanceCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  card: {
+    display: 'flex',
+    maxWidth: 400
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+});
+
+function MediaControlCard(props) {
+  const { classes, theme, balance } = props;
+
+  return (
+    <Card className={classes.card}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            My Address Balance
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {`${balance} BTC`}
+          </Typography>
+        </CardContent>
+      </div>
+    </Card>
+  );
+}
+
+MediaControlCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(MediaControlCard);
